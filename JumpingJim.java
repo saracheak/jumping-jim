723d61d31ca9be367fc0db3c58103a9d6f697f87
saracheak <saracheak@gmail.com>
2025-05-02 19:05:47 -0400

    change input filename

--- JumpingJim.java
+++ JumpingJim.java
@@ -3,12 +3,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
 
-//TODO: can we assume input is correct
-
 public class JumpingJim {
     public static void main(String[] args) throws IOException {
         //read input and put into adjacency list
-        Map<Integer, Node> adjList = ReadInput.read("small.txt");
+        Map<Integer, Node> adjList = ReadInput.read("input.txt");
 
         //get the path of directions
         ArrayList<String> path = Algorithm.bfs(adjList);
